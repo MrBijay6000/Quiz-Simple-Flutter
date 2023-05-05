791e06fc675a3bc61f272ce30042e2de0c6ddc1e
Bijay Shrestha <shresthabzay95@gmail.com>
2023-05-05 21:47:46 +0545

    Second Commit

---
+++ lib/questions_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/answer_button.dart';
+import 'package:first_app/data/questions.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreen();
+  }
+}
+
+class _QuestionsScreen extends State<QuestionsScreen> {
+  @override
+  Widget build(context) {
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
+        ],
+      ),
+    );
+  }
+}
