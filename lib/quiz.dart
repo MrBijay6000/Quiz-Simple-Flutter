05cb99116a132fcc7f2c4b27388a94bee936a5e2
Bijay Shrestha <shresthabzay95@gmail.com>
2023-05-06 15:11:53 +0545

    completed:

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,8 @@
+import 'package:first_app/data/questions.dart';
 import 'package:first_app/questions_screen.dart';
 import 'package:first_app/start_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:first_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -11,6 +13,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   // Widget? activeScreen;
 
   var activeScreen = 'start-screen';
@@ -33,12 +36,30 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
   @override
   Widget build(context) {
     Widget screenWidget = StartScreen(switchScreen);
 
     if (activeScreen == 'questions-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
     }
 
     return MaterialApp(
